d383fb864dd54686d9ed07e75ab1d9d8cade7661
DWER <dianwei_fang@163.com>
2022-05-11 17:21:07 +0800

    新增更新终端ICCID接口

--- whitelistService/src/main/java/apijson/controller/ApiController.java
+++ whitelistService/src/main/java/apijson/controller/ApiController.java
@@ -8,8 +8,8 @@ import apijson.orm.exception.NotExistException;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
-import static apijson.RequestMethod.DELETE;
 import static apijson.RequestMethod.GET;
+import static apijson.RequestMethod.PUT;
 
 /**
  * API Controller
@@ -75,6 +75,13 @@ public class ApiController {
         JSONObject jo1 = new JSONRequest();
         jo1.put("terminal_id", terminalId);
         switch (tag) {
+            //根据终端ID获取终端信息
+            case "Terminal":
+                jo1.remove("terminal_id");
+                jo1.put("id", terminalId);
+                jo.put("Terminal", jo1);
+                response = new JSONResponse(new MyParser(GET, false).parseResponse(jo));
+                break;
             //根据终端ID获取白名单
             case "Whitelist":
                 JSONObject jo2 = new JSONRequest();
@@ -97,5 +104,45 @@ public class ApiController {
         }
 
         return response;
+    }
+
+    /**
+     * 更新终端ICCID
+     *
+     * @param request
+     * @return
+     */
+    @PutMapping("updateIccid")
+    public JSONObject get(@RequestBody String request) {
+        //校验参数
+        JSONObject requestObject = null;
+        String imei, iccid;
+        try {
+            requestObject = MyParser.parseRequest(request);
+            imei = requestObject.getString("imei");
+            iccid = requestObject.getString("iccid");
+        } catch (Exception e) {
+            return MyParser.extendErrorResult(requestObject, e);
+        }
+
+        //根据IMEI获取终端ID
+        Integer terminalId;
+        JSONObject jo = new JSONObject();
+        jo.put("imei", imei);
+        JSONObject jo1 = new JSONRequest();
+        jo1.put("Terminal", jo);
+        JSONResponse resp = new JSONResponse(new MyParser(GET, false).parseResponse(jo1)).getJSONResponse("Terminal");
+        if (resp == null) {
+            return MyParser.newErrorResult(new NotExistException("终端不存在"));
+        }
+        terminalId = resp.getInteger(apijson.JSONObject.KEY_ID);
+
+        //根据终端ID更新终端ICCID
+        jo = new JSONObject();
+        jo.put("id", terminalId);
+        jo.put("iccid", iccid);
+        jo1 = new JSONRequest();
+        jo1.put("Terminal", jo);
+        return new JSONResponse(new MyParser(PUT, false).parseResponse(jo1)).getJSONResponse("Terminal");
     }
 }
